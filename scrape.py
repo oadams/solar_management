73c6166146e652c418d399ccbc194042caeb58f2
Oliver Adams <oliver.adams@gmail.com>
2023-09-30 09:17:52 +1000

    Scraping to file, added argparse.

--- scrape.py
+++ scrape.py
@@ -1,10 +1,16 @@
+import argparse
 import os
 import time
 
+import pandas as pd
 from playwright.sync_api import Playwright, sync_playwright
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--frequency', default=10, type=int, help='Frequency of data collection in seconds')
+parser.add_argument('--outfile', default='data.csv', type=str, help='Output file name')
 
-def run(playwright: Playwright) -> None:
+
+def run(playwright: Playwright, args) -> None:
     browser = playwright.chromium.launch(headless=True)
     context = browser.new_context()
     page = context.new_page()
@@ -12,22 +18,29 @@ def run(playwright: Playwright) -> None:
     page.get_by_role("button", name="Allow all (incl. US-provider cookies)").click()
     page.get_by_role("button", name="Login").click()
     page.get_by_label("Email").click()
-    page.get_by_label("Email").fill(os.environ['fonius_user_email'])
+    page.get_by_label("Email").fill(os.environ['FRONIUS_USER_EMAIL'])
     page.get_by_label("Password").click()
-    page.get_by_label("Password").fill(os.environ['fronius_password'])
+    page.get_by_label("Password").fill(os.environ['FRONIUS_PASSWORD'])
     page.get_by_role("button", name="Login").click()
-    for _ in range(10):
-        time.sleep(10)
+    df = pd.DataFrame(columns=['timestamp', 'pv', 'consumer', 'grid'])
+    while True:
+        time.sleep(args.frequency)
         loc = page.locator("#pvText")
-        print(loc.text_content())
+        pv = loc.text_content()
         loc = page.locator("#consumerText")
-        print(loc.text_content())
-        print('---')
+        consumer = loc.text_content()
+        loc = page.locator("#gridText")
+        grid = loc.text_content()
+        # TODO Set the types of the columns:
+        # - timestamp: datetime
+        # - pv, consumer, grid: float and make them all standardized to Watts or kilowatts.
+        row_df = pd.DataFrame({'timestamp': [time.time()], 'pv': [pv], 'consumer': [consumer], 'grid': [grid]})
+        df = pd.concat([df, row_df])
+        print(df)
+        df.to_csv(args.outfile, index=False)
 
-    # ---------------------
-    context.close()
-    browser.close()
 
-
-with sync_playwright() as playwright:
-    run(playwright)
+if __name__ == '__main__':
+    args = parser.parse_args()
+    with sync_playwright() as playwright:
+        run(playwright, args)
