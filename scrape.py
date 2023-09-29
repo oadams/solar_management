1b663dd78046144691cc23ab8415e39835779635
Oliver Adams <oliver.adams@gmail.com>
2023-09-30 09:49:23 +1000

    normalizing units in dataframe.

--- scrape.py
+++ scrape.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import re
 import time
 
 import pandas as pd
@@ -8,6 +9,22 @@ from playwright.sync_api import Playwright, sync_playwright
 parser = argparse.ArgumentParser()
 parser.add_argument('--frequency', default=10, type=int, help='Frequency of data collection in seconds')
 parser.add_argument('--outfile', default='data.csv', type=str, help='Output file name')
+
+def standardize_to_kw(power_string):
+    """
+    Standardize the units of the power string to kilowatts.
+    """
+
+    match = re.match(r'(\d+\.?\d*)\s*(\w+)', power_string)
+    num = float(match.group(1))
+    units = match.group(2)
+    if units == 'kW':
+        return num
+    elif units == 'W':
+        return num / 1000
+    else:
+        raise ValueError(f'Unknown units: {units}')
+
 
 
 def run(playwright: Playwright, args) -> None:
@@ -31,12 +48,10 @@ def run(playwright: Playwright, args) -> None:
         consumer = loc.text_content()
         loc = page.locator("#gridText")
         grid = loc.text_content()
-        # TODO Set the types of the columns:
-        # - timestamp: datetime
-        # - pv, consumer, grid: float and make them all standardized to Watts or kilowatts.
-        row_df = pd.DataFrame({'timestamp': [time.time()], 'pv': [pv], 'consumer': [consumer], 'grid': [grid]})
+        row_df = pd.DataFrame({'timestamp': [time.time()], 'pv': [standardize_to_kw(pv)], 'consumer': [standardize_to_kw(consumer)], 'grid': [standardize_to_kw(grid)]})
         df = pd.concat([df, row_df])
-        print(df)
+        row_df['timestamp'] = pd.to_datetime(row_df['timestamp'], unit='s')
+        print(row_df)
         df.to_csv(args.outfile, index=False)
 
 
